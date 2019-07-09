20ab07e76bd075df731c342c8e22560767047439
JeesooHaa <hotissue643@gmail.com>
2019-07-09 12:50:36 +0900

    StartCamp Day 02

---
+++ startCamp/02_day/01_naver_rank.py
@@ -0,0 +1,14 @@
+import requests
+import bs4
+
+url = 'https://www.naver.com/'
+
+response = requests.get(url)
+
+html = response.text
+
+soup = bs4.BeautifulSoup(html, 'html.parser')
+rank = soup.select('.ah_l .ah_item .ah_a .ah_k ')
+
+for i in rank[:20]:
+    print(i.text)
